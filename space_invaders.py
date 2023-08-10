bdc6f2e630c87cb969977fe993cc77c8f975ec11
anzhelika17 <akostyuk@conestogac.on.ca>
2023-08-10 18:41:47 -0400

    Added shooting functionality to the game

--- space_invaders.py
+++ space_invaders.py
@@ -1,6 +1,7 @@
 import pygame
 import components.controls
 from components.gun import Gun
+from pygame.sprite import Group
 
 def start():
     pygame.init()
@@ -8,12 +9,13 @@ def start():
     pygame.display.set_caption("Space Invaders")
     bg_color = (0, 0, 0)
     gun = Gun(screen)
+    bullets = Group()
 
     while True:
-        components.controls.events(gun)
+        components.controls.events(screen, gun, bullets)
         gun.update_gun()
-        screen.fill(bg_color)
-        gun.show()
-        pygame.display.flip()
+        components.controls.update(bg_color, screen, gun, bullets)
+        components.controls.update_bullets(bullets)
+
 
 start()
