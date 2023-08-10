710b9f7685e03e000f51cfbcac5b5cd08a23b1f5
anzhelika17 <akostyuk@conestogac.on.ca>
2023-08-10 17:08:51 -0400

    First commit for the space invaders game developed on Python

---
+++ space_invaders.py
@@ -0,0 +1,21 @@
+import pygame
+import sys
+from components.gun import Gun
+
+def start():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Space Invaders")
+    bg_color = (0, 0, 0)
+    gun = Gun(screen)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        gun.show()
+        pygame.display.flip()
+
+start()
